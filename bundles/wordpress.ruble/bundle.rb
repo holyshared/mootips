55cbc8cc248351e0dcfc98b46d942d4ad1047276
Noritaka Horio <holy.shared.design@gmail.com>
2011-03-16 23:14:36 +0900

    remove bundle
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- bundles/wordpress.ruble/bundle.rb
+++
@@ -1,9 +0,0 @@
-require 'ruble'
-
-bundle do |bundle|
-	bundle.display_name = "Wordpress"
-	bundle.author = "Noritaka Horio<holy.shared.design@gmail.com>"
-	bundle.description = ""
-	bundle.copyright = "Copyright 2010-2011 Noritaka Horio All rights reserved."
-	bundle.repository = "http://sharedhat.com"
-end
